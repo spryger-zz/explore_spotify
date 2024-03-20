a9ba426886632f943a3db0cc59427e6894d829e7
Brian Cross <brian.cross741@gmail.com>
2024-03-20 16:02:27 -0400

    SQL scripts, new analyze folder and VIEW for basic stream data

--- SQL Scripts/analyze/Results - Newly played artists and albums by month.sql
+++ SQL Scripts/analyze/Results - Newly played artists and albums by month.sql
@@ -1,14 +1,28 @@
-select * from (
-select artist_name, first_played_month, ms_played ,
-ROW_NUMBER() OVER (PARTITION BY (first_played_month) ORDER BY ms_played DESC) row_count
-from new_artists_streamed_monthly nasm 
-) a
+select * 
+from (
+	select artist_name, 
+		main_genre, 
+		first_played_month,
+		min_streamed_total,
+		min_streamed_in_month,
+		hr_streamed_total,
+		hr_streamed_in_month,
+		ROW_NUMBER() OVER (PARTITION BY (first_played_month) ORDER BY min_streamed_in_month DESC) row_count
+	from new_artists_streamed_monthly nasm 
+	) a
 where row_count =1
 
-select * from (
-select album_name, first_played_month, ms_played ,
-ROW_NUMBER() OVER (PARTITION BY (first_played_month) ORDER BY ms_played DESC) row_count
-from new_albums_streamed_monthly nasm 
-) a
+select * 
+from (
+	select album_name, 
+		artist_name,
+		main_genre,
+		first_played_month, 
+		min_played ,
+		ROW_NUMBER() OVER (PARTITION BY (first_played_month) ORDER BY min_played DESC) row_count
+	from main.new_albums_streamed_monthly nasm 
+	) a
 where row_count =1
 
+
+SELECT * FROM streams s WHERE artist_name = 'TOOL' ORDER BY played_at limit 10
